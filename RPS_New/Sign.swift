769309e4e8232b2d8a25fc5271b6433f372151f4
eduardo salcedo <salcedo.eduardo@gmail.com>
2025-02-06 09:09:12 -0500

    Add GameState.wsift and logic to Sign.swift

--- RPS_New/Sign.swift
+++ RPS_New/Sign.swift
@@ -1,5 +1,17 @@
 import Foundation
 
+func randomSign() -> Sign {
+    let sign = Int.random(in: 0...2)
+    if sign == 0 {
+        return .rock
+    } else if sign == 1 {
+        return .paper
+    } else {
+        return .scissors
+    }
+}
+
+
 enum Sign {
     case rock
     case paper
@@ -15,4 +27,26 @@ enum Sign {
                 return "✌️"
         }
     }
+    
+    func compare(_ opponentSign: Sign) -> GameState {
+        if self == opponentSign {
+            return .draw
+        }
+        switch self {
+            case .rock:
+                if opponentSign == .scissors {
+                    return .win
+                }
+            case .paper:
+                if opponentSign == .rock {
+                    return .win
+                }
+            case . scissors:
+                if opponentSign == .paper {
+                    return .win
+                }
+        }
+        return .lose
+    }
+    
 }
